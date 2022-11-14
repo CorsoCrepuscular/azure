e3e205eaccf8ce071faf86b2e2c00ea4b3ce9ed9
CorsoCrepuscular <105444426+CorsoCrepuscular@users.noreply.github.com>
2022-11-14 13:58:26 +0100

    Update urls.py

--- serie/urls.py
+++ serie/urls.py
@@ -5,3 +5,19 @@ urlpatterns = [
     path('gestion/', include('gestion.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+
+
+from django.urls import path
+
+from gestion import views
+
+urlpatterns=[
+    path('formulario_modificar', views.formulario_modificar, name='formulario_modificar'),
+    path('formulario_borrar', views.formulario_borrar, name='formulario_borrar'),
+    path('borrar', views.borrar, name='borrar'),
+    path('menu', views.menu, name='menu'),
+    path('listado', views.listado, name='listado'),
+    path('',views.index,name='index')
+]
