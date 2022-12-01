809c0546f03af6b0161aff232eac46cbda632ff2
CorsoCrepuscular <105444426+CorsoCrepuscular@users.noreply.github.com>
2022-12-01 20:31:45 +0100

    Add files via upload

---
+++ 7_MVC_Doctores/DoctoresController.cs
@@ -0,0 +1,54 @@
+﻿using Ej7MantMvcDoctores.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ej7MantMvcDoctores.Controllers
+{
+    public class DoctoresController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            ModeloDoctor modelo = new ModeloDoctor();
+            List<DOCTOR> lista = modelo.MostrarDoctores();             
+            return View(lista);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ModeloDoctor modelo = new ModeloDoctor();
+            DOCTOR doc = modelo.EditarDoctor(id);
+            return View(doc);
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        public ActionResult Create(String hosCod, String docNum, String ape, String esp, int sal)
+        {
+            ModeloDoctor modelo = new ModeloDoctor();
+            modelo.InsertarDoctor(hosCod, docNum, ape, esp, sal);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(String hosCod, String docNum, String ape, String esp, int sal)
+        {
+            ModeloDoctor modelo = new ModeloDoctor();
+            modelo.ModificarDoctor(hosCod, docNum, ape, esp, sal);
+            return RedirectToAction("Index");
+        }
+
+       
+        public ActionResult Destroy(int id)
+        {
+            ModeloDoctor modelo = new ModeloDoctor();
+            modelo.EliminarDoctor(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 7_MVC_Doctores/ModeloDoctor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ej7MantMvcDoctores.Models
+{
+    public class ModeloDoctor
+    {
+        DoctorDataContext contexto = new DoctorDataContext();
+
+        public List<DOCTOR> MostrarDoctores()
+        {
+            var datos = from doc in contexto.DOCTOR
+                        select doc;
+            return datos.ToList();
+        }
+
+        public void EliminarDoctor(int id)
+        {
+            DOCTOR dato = (from doc in contexto.DOCTOR
+                           where doc.DOCTOR_NO == id.ToString()
+                           select doc).First();
+            contexto.DOCTOR.DeleteOnSubmit(dato);
+            contexto.SubmitChanges();                        
+                          
+        }
+
+        public void InsertarDoctor(String hosCod, String docNum, String ape, String esp, int sal)
+        {
+            DOCTOR doc = new DOCTOR();
+            doc.HOSPITAL_COD = hosCod;
+            doc.DOCTOR_NO = docNum;
+            doc.APELLIDO = ape;
+            doc.ESPECIALIDAD = esp;
+            doc.SALARIO = sal;
+
+            contexto.DOCTOR.InsertOnSubmit(doc);
+            contexto.SubmitChanges();
+        }
+
+        public void ModificarDoctor(String hosCod, String docNum, String ape, String esp, int sal)
+        {
+            DOCTOR doctor = (from doc in contexto.DOCTOR
+                           where doc.DOCTOR_NO == docNum
+                           select doc).First();
+            doctor.HOSPITAL_COD = hosCod;
+            doctor.APELLIDO = ape;
+            doctor.ESPECIALIDAD = esp;
+            doctor.SALARIO = sal;
+
+            contexto.SubmitChanges();
+        }
+
+        public DOCTOR EditarDoctor(int id)
+        {
+            DOCTOR doctor = (from doc in contexto.DOCTOR
+                             where doc.DOCTOR_NO == id.ToString()
+                             select doc).First();
+            return doctor;
+        }
+
+    }
+}
