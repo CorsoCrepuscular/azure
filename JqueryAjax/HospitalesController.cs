608ee53a04f337151fab5b50c22632c765a50cda
CorsoCrepuscular <105444426+CorsoCrepuscular@users.noreply.github.com>
2022-12-02 20:31:22 +0100

    Add files via upload

---
+++ JqueryAjax/HospitalesController.cs
@@ -0,0 +1,41 @@
+﻿using JqueryAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryAjax.Controllers
+{
+    public class HospitalesController : Controller
+    {
+        DatosDataContext contexto = new DatosDataContext();
+
+        List<HOSPITAL> listahospitales;
+        List<DOCTOR> listadoctores;    
+        List<PLANTILLA> listaplantilla; 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RecuperarHospitales()
+        {
+            ModeloDatos m=new ModeloDatos();
+            listahospitales = m.devolverDatosHospital();
+            return View("_ListaHospitales", this.listahospitales);
+        }
+        public ActionResult RecuperarDoctores()
+        {
+            ModeloDatos m= new ModeloDatos();
+            listadoctores = m.devolverDatosDoctores();
+            return View("_ListaDoctores", this.listadoctores);
+            
+        }
+        public ActionResult RecuperarPlantilla()
+        {
+            ModeloDatos m = new ModeloDatos();
+            listaplantilla = m.devolverDatosPlantilla();
+            return View("_ListaPlantilla", this.listaplantilla);
+        }
+    }
+}
---
+++ JqueryAjax/ModeloDatos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JqueryAjax.Models
+{
+    public class ModeloDatos
+    {
+        DatosDataContext contexto = new DatosDataContext();
+
+        public List<HOSPITAL> devolverDatosHospital() {
+
+            var datos = from hos in contexto.HOSPITAL
+                        select hos;
+            return datos.ToList();
+        }
+
+        public List<DOCTOR> devolverDatosDoctores()
+        {
+            var datos = from doc in contexto.DOCTOR
+                        select doc;
+            return datos.ToList();
+        }
+
+        public List<PLANTILLA> devolverDatosPlantilla()
+        {
+            var datos = from pla in contexto.PLANTILLA
+                        select pla;
+            return datos.ToList();
+        }
+    }
+}
