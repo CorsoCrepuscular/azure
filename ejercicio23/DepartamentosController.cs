8ed700dfd0bd72fa22c18aa9427630834fa2f5bb
CorsoCrepuscular <105444426+CorsoCrepuscular@users.noreply.github.com>
2023-02-28 12:37:06 +0100

    Add files via upload

---
+++ ejercicio23/DepartamentosController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcDockersMySqlV6.Repositories;
+
+namespace MvcDockersMySqlV6.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private RepositoryDepartamentos repo;
+
+        public DepartamentosController(RepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(this.repo.GetDepartamentos());
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(this.repo.FindDepartamento(id));
+        }
+
+        [HttpPost]
+        public IActionResult Insertar(int id, string nom, string loc)
+        {
+            this.repo.InsertarDepartamento(id, nom, loc);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Eliminar(int id)
+        {
+            this.repo.EliminarDepartamento(id);  
+            return RedirectToAction ("Index");
+            
+        }
+
+        [HttpPost]
+        public IActionResult Modificar(int id, string nom, string loc)
+        {
+            this.repo.EditDepartamento(id, nom, loc);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ ejercicio23/RepositoryDepartamentos.cs
@@ -0,0 +1,51 @@
+﻿using MvcDockersMySqlV6.Data;
+using MvcDockersMySqlV6.Models;
+
+namespace MvcDockersMySqlV6.Repositories
+{
+    public class RepositoryDepartamentos
+    {
+        private DepartamentosContext context;
+
+        public RepositoryDepartamentos(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Departamento> GetDepartamentos()
+        {
+            return this.context.Departamentos.ToList();
+        }
+
+        public Departamento FindDepartamento(int id)
+        {
+            return this.context.Departamentos.SingleOrDefault(x => x.IdDepartamento == id);
+        }
+
+        public void EditDepartamento(int id, string nom, string loc) 
+        {
+            Departamento dep = this.FindDepartamento(id);
+;           dep.Nombre = nom;
+            dep.Localidad = loc;
+            this.context.SaveChanges();         
+        }
+
+        public void InsertarDepartamento(int id, string nom, string loc)
+        {
+            Departamento dep = new Departamento();
+            dep.IdDepartamento = id;
+            dep.Nombre = nom;
+            dep.Localidad = loc;
+            this.context.Departamentos.Add(dep);
+            this.context.SaveChanges();
+
+        }
+
+        public void EliminarDepartamento(int id)
+        {
+            Departamento dep = this.FindDepartamento(id);
+            this.context.Departamentos.Remove(dep);
+            this.context.SaveChanges();
+        }
+    }
+}
